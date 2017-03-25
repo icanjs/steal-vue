dba9449cdb97b802db2df65f041c9dee34b8eaf5
Marshall Thompson <marshall@creativeideal.net>
2017-03-25 15:38:49 -0600

    Attempt to load styles

--- steal-vue.js
+++ steal-vue.js
@@ -1,9 +1,31 @@
 const compiler = require('vue-template-compiler');
 
 exports.translate = function (load) {
+  let loader = this.localLoader || this;
   let output = compiler.parseComponent(load.source);
   let script = output.script && output.script.content;
+  let styles = output.styles && output.styles[0] && `"${output.styles[0].content}"`;
   let template = output.template && output.template.content;
+
+  let baseName = load.name.substr(0, load.name.indexOf('!'));
+
+  function makeName (baseName, part, plugin) {
+    return baseName + '-' + part + (plugin ? ('.' + plugin) : '');
+  }
+  function makeAddress (address, part, plugin) {
+    let base = address + '.' + part;
+    return base + (plugin ? ('.' + plugin) : '');
+  }
+
+  // load.metadata.virtualDeps = [];
+
+  debugger;
+  // Load styles
+  let styleName = makeName(baseName, 'style', 'css!');
+  let styleAddress = makeAddress(load.address, 'style', 'css!');
+  loader.define(styleName, `"${styles}"`, {
+    address: styleAddress
+  });
 
   // Inject the template into the script string.
   let templateString = `template: \`${template}\`, `;
@@ -12,3 +34,4 @@ exports.translate = function (load) {
 
   load.source = script;
 };
+
