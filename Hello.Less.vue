525f46ea9afc2e332e725cd8da44513fa55d6198
Marshall Thompson <marshall@creativeideal.net>
2017-03-25 23:35:49 -0600

    Added Less styles support

---
+++ Hello.Less.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="hello-less">{{message}}</p>
+</template>
+
+<script>
+export default {
+  name: 'hello-less',
+  data () {
+    return {
+      message: 'Hello, Vue with Less!'
+    }
+  },
+  methods: {
+    log (value) {
+      console.log(value)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+p {
+  padding: 100px;
+  &.hello-less {
+    color: lightseagreen;
+    font-weight: bold;
+  }
+}
+</style>
--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 import Vue from 'vue/dist/vue';
 import AppHello from './Hello.vue';
+import AppHelloLess from './Hello.Less.vue';
 
 var app = new Vue({
   el: '#app',
-  template: `<AppHello />`,
+  template: `<div><AppHello /><AppHelloLess /></div>`,
   components: {
-    AppHello
+    AppHello,
+    AppHelloLess
   }
 });
